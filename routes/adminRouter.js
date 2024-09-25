5cf69633a77fd6acbebf3845e65aa7a2be7b2a26
Preethi <preethisreejit@gmail.com>
2024-09-25 12:01:55 +0530

    category offer

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -5,6 +5,7 @@ const {userAuth,adminAuth}=require("../middleware/auth")
 const customerController=require("../controllers/admin/cusomerController")
 
 
+const categoryController=require("../controllers/admin/categoryController")
 router.get("/pageError",adminController.pageError)
 router.get("/login",adminController.loadLogin)
 router.post("/login",adminController.login)
@@ -13,4 +14,9 @@ router.get("/logout",adminController.logout)
 router.get("/users",adminAuth,customerController.customerinfo)
 router.get("/blockCustomer",adminAuth,customerController.customerBlocked);
 router.get("/unblockCustomer",adminAuth,customerController.customerunBlocked);
+router.get("/category",adminAuth,categoryController.categoryInfo);
+router.post("/addCategory",adminAuth,categoryController.addCategory)
+router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
+router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer);
+
 module.exports=router
